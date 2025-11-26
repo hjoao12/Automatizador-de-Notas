2843e2a2d48972ca7f817fe52a473feaebe52fab
hjoao12 <149897327+hjoao12@users.noreply.github.com>
2025-11-26 16:43:50 -0300

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -12,7 +12,7 @@ import pickle
 import base64
 from streamlit_pdf_viewer import pdf_viewer
 from pathlib import Path
-from PyPdf import PdfReader, PdfWriter
+from pypdf import PdfReader, PdfWriter
 import google.generativeai as genai
 from google.api_core.exceptions import ResourceExhausted
 import streamlit as st
